b144a87a6412e1e66a7a0217a94c780b5ae398f3
Arsalan Ibrahimi <arsalanibrahimi850@gmail.com>
2024-11-23 15:23:07 +1030

    desktop view for replacing chrome default launcher

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.action.onClicked.addListener(function (tab) {
-    console.log('hello');
-});
+chrome.tabs.onCreated.addListener(function(tab) {
+    if (tab.url === 'chrome://newtab/') {
+      console.log("New tab opened!");
+  
+      // Your script or function to run when a new tab is opened
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ['content.js']  // script you want to run on new tab
+      });
+    }
+  });
---
+++ content.js
@@ -0,0 +1,5 @@
+
+
+let body = document.querySelector('body');
+
+body.innerHTML = '<h1>Your new Page</h1>';
